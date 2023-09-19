95f3bda82150d6e75f8ca216e089e9615c2b5d9e
ArnauvGilotra <arnauvgilotra@gmail.com>
2023-09-19 19:42:01 -0400

    Added email check and flash messages

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT email address?', validators=[Email(), DataRequired()])
     submit = SubmitField('Submit')
 
 app = Flask(__name__)
@@ -19,10 +20,19 @@ moment = Moment(app)
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        session['name'] = form.name.data
-        return redirect(url_for('index'))
+        old_name = session.get('name')
+        old_name, old_email = session.get('name'), session.get('email')
+        if old_name and old_name != form.name.data:
+            flash("Looks like you have changed your name!")
 
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+        if old_email and old_email != form.email.data:
+            flash("Looks like you have changed your email!")
+
+        session['name'] = form.name.data 
+        session['email'] = form.email.data
+
+    return render_template('index.html', form=form,
+     name=session.get('name'), email=session.get('email'), current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
